88ff26f71b76977cb8da754b680e1b688ed89196
Denis Adamchik <denis.adamchik@gmail.com>
2024-06-22 10:52:23 +0300

    Add integration specs

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -3,11 +3,12 @@ require 'json'
 require_relative '../../app/api'
 
 module ExpenseTracker
-  RSpec.describe 'Expense Tracker API' do
+  RSpec.describe 'Expense Tracker API', :db do
     include Rack::Test::Methods
 
     def app
-      ExpenseTracker::API.new
+      API.ledger = Ledger.new
+      API.new
     end
 
     def post_expense(expense)
@@ -20,8 +21,6 @@ module ExpenseTracker
     end
 
     it 'records submitted expenses' do
-      pending 'Need to persist expenses'
-
       coffee = post_expense(
         'payee'  => 'Starbucks',
         'amount' => 5.75,
